787d8c87aac04654baadeed2d11def7c9dda06b7
Andrew Nowak <an8@sanger.ac.uk>
2017-03-01 09:21:04 +0000

    Simple server now takes port & code on command line

--- serv404.py
+++ serv404.py
@@ -2,22 +2,42 @@
 
 import BaseHTTPServer
 import sys
+import argparse
 
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_error(404, 'Custom fourohfour')
+        self.send_error(code, message)
         return
 
     def do_POST(self):
-        self.send_error(404, 'Custom fourohfour')
+        self.send_error(code, message)
         return
 
+
 def main():
-    port = 8000
+    global code, message
+    desc = "Runs a small http server. Returns 404 (or your "\
+           "choice of code) on all GET and POST requests."
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('--port', dest='port', action='store',
+            default=8000, type=int,
+            help="Run server on this port")
+    parser.add_argument('--code', dest='code', action='store',
+            default=404, type=int,
+            help="Return this code on all requests")
+    parser.add_argument('--message', dest='message', action='store',
+            default='', type=str,
+            help="Send this message on all requests")
+    args = parser.parse_args()
+    port = args.port
+    code = args.code
+    message = args.message
+    if len(message) < 1:
+        message = 'Custom {}'.format(code)
     server_address = ('', port)
     httpd = BaseHTTPServer.HTTPServer(server_address, MyHandler)
-    print 'serving 404s on port', port
+    print 'serving {}s on port {}'.format(code, port)
     httpd.serve_forever()
 
 if __name__ == '__main__':
